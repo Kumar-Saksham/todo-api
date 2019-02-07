a28cb2e5263965904a5dcd5b27fb55e1f51da775
Saksham Kumar <sakshamkumar@Sam.local>
2019-02-07 17:27:36 +0530

    Added GET /todos/:id test cases

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,17 +1,22 @@
 const expect = require("expect");
 const request = require("supertest");
+const { ObjectId } = require("mongodb");
 
 const { app } = require("./../server");
 const { Todo } = require("./../models/todo");
 
 const todos = [
   {
-    text: "First test todo"
+    text: "First test todo",
+    _id: new ObjectId()
   },
   {
-    text: "Second test todo"
+    text: "Second test todo",
+    _id: new ObjectId()
   }
 ];
+
+console.log(todos);
 
 beforeEach(done => {
   Todo.remove({})
@@ -80,3 +85,39 @@ describe("GET /todos", () => {
             .end(done);
     });
 });
+
+describe("GET /todos/:id", () => {
+    const correct_id = todos[0].id;
+    const invalid_id = '124';
+    const absent_id = "6c5c060b33540f07daa4669d";
+
+    it("should return status 404 with empty body on invalid id", done => {
+    request(app)
+        .get(`/todos/${invalid_id}`)
+        .expect(404)
+        .end(done);
+    });
+
+    it("should return status 400 with empty body on valid but absent id", done => {
+        request(app)
+            .get(`/todos/${absent_id}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it("should return correct todo on valid present id", done => {
+        request(app)
+            .get(`/todos/${todos[0]._id}`)
+            .expect(200)
+            .end((err, res) => {
+                if(err) {
+                    return done(err);
+                }
+                Todo.find({ _id: todos[0]._id }).then(todos => {
+                    expect(todos.length).toBe(1);
+                    expect(res.body.todo.text).toBe(todos[0].text);
+                    done();
+                }).catch(e => done(e));
+            })
+    })
+})
